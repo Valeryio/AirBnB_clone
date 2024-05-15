dd51a5deae5b8bad8ab80de9f2799fa5f71103bd
Valeryio <tommagoma23@gmail.com>
2024-05-15 04:14:01 +0000

    Updating the date attributes

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import uuid
+from datetime import datetime
 
 """
     This is the module for the base model of all the class we will use
@@ -13,20 +14,24 @@ class BaseModel():
 
     Attributes:
        id (str) : an uid assigned to each instance created
+       created_at (datetime) : the date of creation of the instance
+       updated_at (datetime) : the date of updating of the instance
     """
 
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.isoformat(datetime.today())
 
+    def __str__(self):
+        return "{self.__name__} + {self.id} <{self.__dict__}>"
 
-    def created_at(self):
+    def to_dict(self):
         """
-            This public method assign the current datetime when an instance
-            is created
+            Change to dict the arg of the public instance
         """
         pass
 
-    def updated_at(self):
+    def save(self):
         """
             This public method assign the current datetime when an instance
             is created and it will be updated every time the object changes.
--- models/main.py
+++ models/main.py
@@ -4,4 +4,4 @@ from base_model import BaseModel
 
 user1 = BaseModel()
 
-print("Voici son id : ", user1.id)
+print("Voici son id : ", user1.id, user1.created_at)
