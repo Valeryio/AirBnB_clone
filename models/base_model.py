63be12041d402773d64c18556d64235e01feff75
valeryio <tommagoma23@gmail.com>
2024-05-15 17:11:04 +0100

    Updated the base model class for the task 3. Datetime attributes have been fixed!

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,25 @@ class BaseModel():
        updated_at (datetime) : the date of updating of the instance
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.isoformat(datetime.today())
+    def __init__(self, *args, **kwargs):
+
+        dformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            known_args = ["created_at", "updated_at"]
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in known_args:
+                        value = datetime.strptime(value, dformat)
+
+                    self.__setattr__(key, value)
+                    print(f"{key} ----- {value}")
+            print("HERE WE GO")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            #self.created_at = datetime.isoformat(datetime.today())
+            #self.updated_at = datetime.isoformat(datetime.today())
+
 
     def __setattr__(self, name, value):
         """
@@ -28,13 +44,14 @@ class BaseModel():
             created, so I modified it to update the instance each time
             this one is modified (have a new attribute by example)
         """
-        if name != "updated_at":
+
+        #if name != "updated_at":
         """
             This condition prevent recursion Error cause, the save() method
             update the attribute updated_at, and it calls the __setattr__
             dunder's method. Prevent the recursion with a condition
         """
-            self.save()
+        self.save()
         super().__setattr__(name, value)
 
     def __str__(self):
@@ -44,12 +61,20 @@ class BaseModel():
         """
             Change to dict the arg of the public instance
         """
-        self.__dict__["__class__"] = type(self).__name__
-        return self.__dict__
+        dtime_args = ["created_at", "updated_at"]
+        new_dict = self.__dict__
+
+        for i in dtime_args:
+            new_dict[i] = datetime.isoformat(new_dict[i])
+
+        new_dict["__class__"] = type(self).__name__
+        return new_dict
 
     def save(self):
         """
             This public method assign the current datetime when an instance
             is created and it will be updated every time the object changes.
         """
-        self.updated_at = datetime.isoformat(datetime.today())
+        super().__setattr__("updated_at", datetime.today())
+        #super().__setattr__("updated_at", datetime.isoformat(datetime.today()))
+        #self.updated_at = datetime.isoformat(datetime.today())
