ff27fdcaf589ad1060d7fe25eab640e674251f76
Valeryio <tommagoma23@gmail.com>
2024-05-15 21:24:18 +0100

    Update from useless code

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,20 @@ class BaseModel():
     """
         This is the BaseMOdel class
 
-    Attributes:
-       id (str) : an uid assigned to each instance created
-       created_at (datetime) : the date of creation of the instance
-       updated_at (datetime) : the date of updating of the instance
+        Attributes:
+            id (str) : an uid assigned to each instance created
+            created_at (datetime) : the date of creation of the instance
+            updated_at (datetime) : the date of updating of the instance
     """
 
     def __init__(self, *args, **kwargs):
-
+        """
+            This is the constructor method
+            
+            Attributes:
+                *args (list): a list of attribute
+                **kwargs (dict): a dict of values
+        """
         dformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             known_args = ["created_at", "updated_at"]
@@ -27,32 +33,11 @@ class BaseModel():
                 if key != "__class__":
                     if key in known_args:
                         value = datetime.strptime(value, dformat)
-
                     self.__setattr__(key, value)
-                    print(f"{key} ----- {value}")
-            print("HERE WE GO")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
-            #self.created_at = datetime.isoformat(datetime.today())
-            #self.updated_at = datetime.isoformat(datetime.today())
-
-
-    def __setattr__(self, name, value):
-        """
-            This method is called each time an atttribute of instance is
-            created, so I modified it to update the instance each time
-            this one is modified (have a new attribute by example)
-        """
-
-        #if name != "updated_at":
-        """
-            This condition prevent recursion Error cause, the save() method
-            update the attribute updated_at, and it calls the __setattr__
-            dunder's method. Prevent the recursion with a condition
-        """
-        self.save()
-        super().__setattr__(name, value)
+            self.updated_at = datetime.today()
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
@@ -75,6 +60,4 @@ class BaseModel():
             This public method assign the current datetime when an instance
             is created and it will be updated every time the object changes.
         """
-        super().__setattr__("updated_at", datetime.today())
-        #super().__setattr__("updated_at", datetime.isoformat(datetime.today()))
-        #self.updated_at = datetime.isoformat(datetime.today())
+        self.updated_at = datetime.today()
