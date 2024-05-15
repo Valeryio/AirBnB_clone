ba06a5ebcb3b9a0bbaeb323aee818c2d57e8883c
valeryio <tommagoma23@gmail.com>
2024-05-15 14:26:09 +0100

    Update of the basemodel class The base model class have been updated. I added a customisation for the __setattr__ dunder's method to track the modifications made on the instance of a class.
    I used super().__setattr__() and not self.__setattr__() to make this
    caracteristic inherits through all the child classes

--- models/base_model.py
+++ models/base_model.py
@@ -22,14 +22,28 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.isoformat(datetime.today())
 
+    def __setattr__(self, name, value):
+        """
+            This method is called each time an atttribute of instance is
+            created, so I modified it to update the instance each time
+            this one is modified (have a new attribute by example)
+        """
+        if name != "updated_at":
+        """
+            This condition prevent recursion Error cause, the save() method
+            update the attribute updated_at, and it calls the __setattr__
+            dunder's method. Prevent the recursion with a condition
+        """
+            self.save()
+        super().__setattr__(name, value)
+
     def __str__(self):
-        return f"{type(self).__name__} {self.id} {self.__dict__}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def to_dict(self):
         """
             Change to dict the arg of the public instance
         """
-        #return self.__dict__
         self.__dict__["__class__"] = type(self).__name__
         return self.__dict__
 
