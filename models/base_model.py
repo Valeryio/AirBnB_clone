5b7ea438cbedf90b1f3f543616be769d91fa43e5
Valeryio <tommagoma23@gmail.com>
2024-05-15 02:54:46 +0000

    First draft of the base model class

---
+++ models/base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    This is the module for the base model of all the class we will use
+    through this project
+"""
+
+class BaseModel():
+    """
+        This is the BaseMOdel class
+
+    Attributes:
+       id (str) : an uid assigned to each instance created
+    """
+
+    def __inti__(self):
+        pass
