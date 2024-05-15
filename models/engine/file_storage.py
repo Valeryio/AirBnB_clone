8abf3c9e74616a206a5ae0f50cd74078241be19b
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-05-15 13:09:20 -0700

    Basemodel

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """A class for serializing and deserializing objects to a JSON file."""
 
@@ -24,11 +25,10 @@ class FileStorage:
             json.dump(self.__objects, f)
 
     def reload(self):
-        """Deserializes the JSON file back to the objects dictionary (if it exists)."""
+        """Deserializes the JSON file
+        back to the objects dictionary (if it exists)."""
         try:
             with open(self.__file_path, "r") as f:
                 self.__objects = json.load(f)
         except FileNotFoundError:
             pass  # Ignore if file doesn't exist
-
-
