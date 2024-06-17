e321bd4438c1ff2fe437d2107448abfb8210e5fe
Valeryio <tommagoma23@gmail.com>
2024-06-17 15:22:59 +0000

    Update thee console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 """This is the module about the console of HBNB"""
 
 import cmd
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,40 @@ class HBNBCommand(cmd.Cmd):
         @prompt:(str), the prompt for the beginning of the line
     """
     prompt = "(hbnb) "
+    all_classes = ["BaseModel", "FileStorage", ""]
+
+    def do_create(self, line):
+        """This methods creates a new instance of BaseModel and
+        save it to a json file"""
+        if line == "":
+            print("** class name missing **")
+        elif line not in self.all_classes:
+            print("** class doesn't exist **")
+        else:
+            new_model = BaseModel()
+            new_model.save()
+
+    def do_show(self, line):
+        """This method prints the string representation of an instance based
+        on the class name and the id"""
+
+        line_parameters = line.split(' ')
+        if line == "":
+            print("** class name missing **")
+
+        if len(line_parameters) >= 2:
+            print(line)
+            if line_parameters[0] not in self.all_classes:
+                print("** class doesn't exist **")
+            elif line_parameters[1] == "":
+                print("** instance id missing **")
+            else:
+                all_objects = storage.all()
+                if all_objects[line_parameters[1]] is None:
+                    print("** no instance found **")
+                else:
+                    print(all_objects[line_parameters[1]])
+
 
     def do_quit(self, line):
         """This method quits the interpreter"""
