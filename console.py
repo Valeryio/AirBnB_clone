851e7c65b46cbe1102408d7aec7126f54d3dcec2
Valeryio <tommagoma23@gmail.com>
2024-06-17 11:50:11 +0000

    THird draft of the Airbnb console

--- console.py
+++ console.py
@@ -14,11 +14,11 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """This method quits the interpreter"""
         print("Quit command to exit the program")
-        return True
+        exit()
 
     def do_EOF(self, line):
         """This is the end of file program that quits the cmd"""
-        return True
+        exit()
 
 
 if __name__ == "__main__":
