5596f77193075083c406ba6006e36c371b3cae83
Valeryio <tommagoma23@gmail.com>
2024-07-15 13:37:29 +0000

    I wrote a simple test for the FileStorage.__objects attribute!

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,5 +1,6 @@
 
 import unittest
+from models.engine.file_storage import FileStorage
 
 """
     This is the test file for the file_storage class
@@ -7,4 +8,14 @@ import unittest
 
 
 class TestFileStorage(unittest.TestCase):
-    pass
+    """This is the test class for the class FileStorage
+    """
+
+    def setUp(self):
+        """This method sets up all the needed attributes
+        for the others tests"""
+        self.disk_1 = FileStorage()
+
+    def test_objects(self):
+        """This test checks for the __objects attribute"""
+        self.assertEquals(self.disk_1.objects, {})
